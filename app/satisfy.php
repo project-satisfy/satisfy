6493d9802fc1d06fed1c060ee00a245eefae7b7e
Julius Beckmann <github@h4cc.de>
2013-06-23 22:35:06 +0300

    Showing satis index.html as index. Satisfy backend available at /admin/
    First: The Code in "$app->get('/', function() use ($app) {...}" does not work, because the home.html.twig needs the 'repositories' variable.
    
    Second:
    In my case, we have a satis repository running and developers are using the index.html generated by satis. The current code does not allow access to that generated index.html.
    So i moved the satisfy backend to /admin/ and added a small proxy for the index.html with a useable error message.
    
    @ludofleury May this be something to merge in?

--- app/satisfy.php
+++ app/satisfy.php
@@ -3,12 +3,19 @@
 $app = require_once __DIR__.'/bootstrap.php';
 
 /**
- * Homepage, lists repositories
+ * Homepage, shows satis index.html file if available.
  */
 $app->get('/', function() use ($app) {
-    return $app['twig']->render('home.html.twig');
+        $indexPath = __DIR__.'/../web/index.html';
+        if (file_exists($indexPath)) {
+            $content = file_get_contents($indexPath);
+        }else{
+            $content = '<h1>Composer Repository currently not available.</h1>';
+        }
+
+        return $content;
 })->bind('home');
 
-$app->mount('/', new \Playbloom\Satisfy\Controller\RepositoryController());
+$app->mount('/admin/', new \Playbloom\Satisfy\Controller\RepositoryController());
 
 return $app;
