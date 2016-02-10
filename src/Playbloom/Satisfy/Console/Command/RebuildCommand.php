85cdfc19f40f2cf77237d3bcb575638e5617c2af
Ramunas <ieskok@ramuno.lt>
2016-02-11 00:00:19 +0200

    add command for rebuild of package definitions when config is updated, fixes #45

---
+++ src/Playbloom/Satisfy/Console/Command/RebuildCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Playbloom\Satisfy\Console\Command;
+
+use Composer\Json\JsonFile;
+use Composer\Satis\Command\BuildCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RebuildCommand extends BuildCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+        $this
+            ->setName('satisfy:rebuild')
+            ->setDescription('Rebuild composer packages when config is changed or definitions is outdated')
+            ->setHelp(null)
+            ->addOption(
+                'lifetime',
+                'l',
+                InputOption::VALUE_OPTIONAL,
+                'Maximum lifetime of composer definitions in seconds'
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $configFile = $input->getArgument('file');
+        $lifetime = (int)$input->getOption('lifetime');
+
+        if (is_file($configFile) && !empty($lifetime)) {
+            if (!$outputDir = $input->getArgument('output-dir')) {
+                $file = new JsonFile($configFile);
+                $config = $file->read();
+                $outputDir = isset($config['output-dir']) ? $config['output-dir'] : null;
+            }
+
+            $modifiedAt = filemtime($configFile);
+            $lastUpdate = @filemtime($outputDir . '/packages.json');
+            if ($modifiedAt < $lastUpdate && time() - $lastUpdate < $lifetime) {
+                return;
+            }
+        }
+
+        parent::execute($input, $output);
+    }
+}
