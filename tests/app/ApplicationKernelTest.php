f582f950aab4456158eba5ce6b18e65b49859305
Ramūnas Dronga <ieskok@ramuno.lt>
2018-02-06 21:55:45 +0200

    fix failing test when index.html already generated

--- tests/app/ApplicationKernelTest.php
+++ tests/app/ApplicationKernelTest.php
@@ -3,6 +3,7 @@
 namespace tests\app;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApplicationKernelTest extends WebTestCase
@@ -10,11 +11,23 @@ class ApplicationKernelTest extends WebTestCase
     public function testUnavailablePageWhenIndexMissing()
     {
         $client = self::createClient();
-        $crawler = $client->request('GET', '/');
-        $response = $client->getResponse();
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $webDir = $client->getContainer()->getParameter('kernel.project_dir').'/web';
+        $filesystem = new Filesystem();
+        if ($filesystem->exists($webDir.'/index.html')) {
+            $filesystem->rename($webDir.'/index.html', $renamed = $webDir.'/_index.html');
+        }
 
-        $title = $crawler->filterXPath('//head/title');
-        $this->assertEquals('Composer Repository currently not available', $title->text());
+        try {
+            $crawler = $client->request('GET', '/');
+            $response = $client->getResponse();
+            $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+            $title = $crawler->filterXPath('//head/title');
+            $this->assertEquals('Composer Repository currently not available', $title->text());
+        } finally {
+            if (isset($renamed)) {
+                $filesystem->rename($renamed, $webDir.'/index.html');
+            }
+        }
     }
 }
