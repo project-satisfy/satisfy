71219e4026133eef325cf00e4f2d3a099a8c587e
Ramūnas Dronga <github@ramuno.lt>
2018-10-08 22:24:03 +0300

    make process output response reusable

--- src/Playbloom/Satisfy/Controller/SatisController.php
+++ src/Playbloom/Satisfy/Controller/SatisController.php
@@ -2,47 +2,25 @@
 
 namespace Playbloom\Satisfy\Controller;
 
+use Playbloom\Satisfy\Http\ProcessResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\Process\Process;
 
 class SatisController extends AbstractProtectedController
 {
-    /**
-     * @return Response
-     */
-    public function buildAction()
+    public function buildAction(): Response
     {
         $this->checkAccess();
 
         return $this->render('@PlaybloomSatisfy/satis_build.html.twig');
     }
 
-    /**
-     * @return StreamedResponse
-     */
-    public function buildRunAction()
+    public function buildRunAction(): Response
     {
         $this->checkAccess();
-
-        ini_set('implicit_flush', 1);
-        ob_implicit_flush(true);
 
         $runner = $this->container->get('satisfy.runner.satis_build');
         $output = $runner->run();
 
-        $processRead = function () use ($output) {
-            foreach ($output as $line) {
-                $this->outputStream($line);
-            }
-            $this->outputStream('__done__');
-        };
-
-        return new StreamedResponse($processRead, Response::HTTP_OK, ['Content-Type' => 'text/event-stream']);
-    }
-
-    protected function outputStream(string $line)
-    {
-        echo 'data: ', $line, PHP_EOL, PHP_EOL;
+        return ProcessResponse::createFromOutput($output);
     }
 }
---
+++ src/Playbloom/Satisfy/Http/ProcessResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Playbloom\Satisfy\Http;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class ProcessResponse extends StreamedResponse
+{
+    public static function createFromOutput(\Iterator $output): self
+    {
+        ini_set('implicit_flush', 1);
+        ob_implicit_flush(true);
+
+        $callback = function () use ($output) {
+            foreach ($output as $line) {
+                self::outputLine($line);
+            }
+            self::outputLine('__done__');
+        };
+
+        return new self($callback, self::HTTP_OK, ['Content-Type' => 'text/event-stream']);
+    }
+
+    protected static function outputLine(string $line)
+    {
+        echo 'data: ', $line, PHP_EOL, PHP_EOL;
+    }
+}
