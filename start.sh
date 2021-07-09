13d19215ef70565bf763ead8722655c41686022c
John Pierce <John.Pierce@stepstonegroup.com>
2021-07-09 16:49:04 -0700

    use DSEBOT_GH_TOKEN

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-composer config --global github-oauth.github.com $GITHUB_TOKEN
+composer config --global github-oauth.github.com $DSEBOT_GH_TOKEN
 chmod +w  /var/www/satisfy/storage/
 ln -s     /var/www/satisfy/storage/satisfy/satis.json /var/www/satisfy/satis.json
 /usr/bin/supervisord -n
