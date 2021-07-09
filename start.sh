c4401850fedc37b2e15083b2f7e68e442cbaeaee
John Pierce <John.Pierce@stepstonegroup.com>
2021-07-09 14:08:35 -0700

    add env github_token

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+composer config --global github-oauth.github.com $GITHUB_TOKEN
 chmod +w  /var/www/satisfy/storage/
 ln -s     /var/www/satisfy/storage/satisfy/satis.json /var/www/satisfy/satis.json
 /usr/bin/supervisord -n
