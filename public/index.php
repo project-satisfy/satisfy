24f0a4467f0e97a40310335958fceff322f7adfb
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2022-07-06 11:35:12 +0300

    Retrieve APP_ENV and APP_DEBUG from environment variables

--- public/index.php
+++ public/index.php
@@ -4,20 +4,27 @@ use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
-if ($_SERVER['APP_DEBUG']) {
+
+$appEnv = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$appDebug = (bool)($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? true);
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+$trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false;
+
+if ($appDebug) {
     umask(0000);
     Debug::enable();
 }
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+if ($trustedProxies) {
     Request::setTrustedProxies(
         explode(',', $trustedProxies),
         Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
     );
 }
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+if ($trustedHosts) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool)($_SERVER['APP_DEBUG'] ?? true));
+
+$kernel = new Kernel($appEnv, $appDebug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
